a0ae73cdb161213e84950c819f8c805b9f37b7a4
John Mosesman <johnmosesman@gmail.com>
2016-04-16 17:21:27 -0500

    Finish up simple three bet game

--- lib/main.ex
+++ lib/main.ex
@@ -1,10 +1,13 @@
 defmodule Main do
+  require IEx
+
   def main(_args \\ nil) do
     IO.puts "Welcome to PokerTrainer."
     get_input
   end
 
   defp get_input do
+    IO.puts "==========================="
     IO.puts "Choose a training exercise:"
     IO.puts "1) Three bet"
     IO.puts "2) Outs calculator"
@@ -13,8 +16,14 @@ defmodule Main do
     input = IO.gets "Enter the number of your choose: "
 
     cond do
-      input =~"1" -> three_bet
-      input =~"2" -> IO.puts "2"
+      input =~"1" ->
+        IO.puts "============="
+        IO.puts "Three bet"
+
+        three_bet
+      input =~"2" ->
+        IO.puts "Coming soon!"
+        get_input
       input =~"3" || String.downcase(input) =~ "exit" -> true
       true -> get_input
     end
@@ -22,13 +31,29 @@ defmodule Main do
 
   defp three_bet do
     IO.puts "============="
-    IO.puts "Three bet"
-    IO.puts "============="
 
     result = Dealer.deal
 
     hand_1 = result |> Map.fetch!(:hand_1)
 
     IO.puts "Your hand is: #{hd(hand_1)}, #{tl(hand_1)}"
+    IO.puts "Do you three bet with tight range?"
+
+    input = IO.gets "(Y/N/Exit): "
+
+    should_three_bet = Trainer.should_three_bet?(hand_1)
+
+    cond do
+      String.downcase(input) =~ "y" ->
+        IO.puts (if should_three_bet, do: "Correct!", else: "Incorrect.")
+        three_bet
+      String.downcase(input) =~ "n" ->
+        IO.puts (if !should_three_bet, do: "Correct!", else: "Incorrect.")
+        three_bet
+      String.downcase(input) =~ "exit" ->
+        true
+      true ->
+        three_bet
+    end
   end
 end
