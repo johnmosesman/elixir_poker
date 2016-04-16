8f4070e539bb20d8e1adb431cb9765aeec1afbdb
John Mosesman <johnmosesman@gmail.com>
2016-04-15 23:00:45 -0500

    Tight 3 bet range test for 9+, A-K

--- lib/trainer.ex
+++ lib/trainer.ex
@@ -1,9 +1,9 @@
 defmodule Trainer do
-  def should_three_bet?([ rank | suit ]) do
-    case [rank] do
-      'A' -> true
-      'K' -> true
-      _   -> false
+  def should_three_bet?([[ rank_1 | suit_1 ], [ rank_2 | suit_2 ]]) do
+    cond do
+      [rank_1] == [rank_2] && Enum.any?('9TJQKA', fn rank -> rank == rank_1 end) -> true
+      ([rank_1] == 'A' && [rank_2] == 'K') || ([rank_1] == 'K' && [rank_2] == 'A') -> true
+      true -> false
     end
   end
 end
--- test/trainer_test.exs
+++ test/trainer_test.exs
@@ -3,8 +3,15 @@ defmodule TrainerTest do
   doctest Trainer
 
   test "should_three_bet?" do
-    assert Trainer.should_three_bet?('AH')
-    assert Trainer.should_three_bet?('KH')
-    refute Trainer.should_three_bet?('QH')
+    # Pocket pairs
+    assert Trainer.should_three_bet?(['9H', '9D'])
+    assert Trainer.should_three_bet?(['JH', 'JD'])
+    assert Trainer.should_three_bet?(['AH', 'AD'])
+    refute Trainer.should_three_bet?(['5H', '5S'])
+
+    # A-K
+    assert Trainer.should_three_bet?(['KH', 'AC'])
+    assert Trainer.should_three_bet?(['AC', 'KS'])
+    refute Trainer.should_three_bet?(['KH', '2S'])
   end
 end
